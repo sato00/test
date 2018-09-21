2ea2cf295fa00b05f8488e1cb56d7781f26b5767
sato00 <sato-chan.812@i.softbank.jp>
2018-09-21 21:59:21 +0900

    9/21オブジェクト指向

---
+++ Person/src/Test.java
@@ -0,0 +1,40 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+//		Person taro=new Person();
+//		taro.name="山田太郎";
+//		taro.age=20;
+//		taro.phoneNumber="080-1234-5678";
+//		taro.address="abc@gmail.com";
+//		System.out.println(taro.name);
+//		System.out.println(taro.age);
+//		System.out.println(taro.phoneNumber);
+//		System.out.println(taro.address);
+//
+//		taro.talk();
+//		taro.walk();
+//		taro.run();
+//
+//
+//		Person jiro=new Person();
+//		jiro.name="木村次郎";
+//		jiro.age=18;
+//		System.out.println(jiro.name);
+//		System.out.println(jiro.age);
+//
+//
+//		Person hanako=new Person();
+//		hanako.name="鈴木花子";
+//		hanako.age=16;
+//		System.out.println(hanako.name);
+//		System.out.println(hanako.age);
+
+
+		Robot aibo=new Robot();
+		aibo.talk();
+
+
+	}
+
+}
