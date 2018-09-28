d77207f5a1554b5997138a473552483852c633b4
菊地 聡美 <sato-chan.812@i.softbank.jp>
2018-09-28 21:31:39 +0900

    9/28継承

---
+++ Person/src/Person.java
@@ -0,0 +1,20 @@
+
+public class Person {
+	private String name=null;
+	private int age=0;
+	
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+public Person() {}
+	
+	public Person(String name, int age) {
+		this.name=name;
+		this.age=age;
+	}
+	
+}
